13684dd769f1989fa20278f92bab17e12b709c11
irfansyahph <irfansyahputrahadiyat@gmail.com>
2022-10-18 15:05:58 +0700

    delete some unused files

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    domain: "http://192.168.1.21:3001",
-};
-
-export default config;
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -7,7 +7,6 @@ export const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case "LOGIN_SUCCESS":
             console.log("DATA DARI ACTION ==>", action.payload)
-            delete action.payload.password
             return { ...state, ...action.payload }
         default:
             return state
